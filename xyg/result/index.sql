15d774094b046d1f81c6b09c81d81738a11e4f98
wangzhefeng <wangzhefengr@163.com>
2020-10-09 00:16:10 +0800

    update dict1.5

--- xyg/result/index.sql
+++ xyg/result/index.sql
@@ -1,177 +1,184 @@
+-- ============================================================
+-- 基础表格
+-- ============================================================
+-- 基础数据下拉菜单
+ALTER TABLE `config_menu` ADD CONSTRAINT fk_config_menu
+FOREIGN KEY(`category_id`) REFERENCES config_category(`id`);
 
-
--- info_modify_record
-ALTER TABLE `info_modify_record` ADD CONSTRAINT fk_info_craft
-FOREIGN KEY(`standard_craft_card_id`) REFERENCES info_craft(`id`);
-
--- card_modify_record
-ALTER TABLE `card_modify_record` ADD CONSTRAINT fk_card_craft
-FOREIGN KEY(`product_craft_card_id`) REFERENCES card_craft(`id`);
-
-
-
--- info_modify_record
-ALTER TABLE `basic_curve_steel_group_relation` ADD CONSTRAINT fk_basic_curve_id
+-- 基础曲线点
+ALTER TABLE `basic_curve_point` ADD CONSTRAINT fk_basic_curve_point
 FOREIGN KEY(`basic_curve_id`) REFERENCES basic_curve(`id`);
 
--- card_modify_record
-ALTER TABLE `basic_curve_steel_group_relation` ADD CONSTRAINT fk_steel_group_id
+-- 基础曲线-钢组关系表
+ALTER TABLE `basic_curve_steel_group_relation` ADD CONSTRAINT fk_basic_curve_steel_group_relation_basic_curve_id
+FOREIGN KEY(`basic_curve_id`) REFERENCES basic_curve(`id`);
+
+ALTER TABLE `basic_curve_steel_group_relation` ADD CONSTRAINT fk_basic_curve_steel_group_relation_steel_group_id
 FOREIGN KEY(`steel_group_id`) REFERENCES basic_steel_group(`id`);
 
+-- 基础曲线-树形菜单
+ALTER TABLE `basic_curve_menu` ADD CONSTRAINT fk_basic_curve_menu_basic_curve_id
+FOREIGN KEY(`basic_curve_id`) REFERENCES basic_curve(`id`);
 
+ALTER TABLE `basic_curve_menu` ADD CONSTRAINT fk_basic_curve_menu_category_id
+FOREIGN KEY(`category_id`) REFERENCES config_category(`id`);
 
+-- 钢号表
+ALTER TABLE `basic_steel_no` ADD CONSTRAINT fk_basic_steel_no
+FOREIGN KEY(`steel_group_id`) REFERENCES basic_steel_group(`id`);
+
+-- 调质工艺曲线树形菜单
+ALTER TABLE `craft_curve` ADD CONSTRAINT fk_craft_curve_info_tempering_id
+FOREIGN KEY(`info_tempering_id`) REFERENCES info_craft_tempering(`id`);
+
+ALTER TABLE `craft_curve` ADD CONSTRAINT fk_craft_curve_card_tempering_id
+FOREIGN KEY(`card_tempering_id`) REFERENCES card_craft_tempering(`id`);
+
+ALTER TABLE `craft_curve` ADD CONSTRAINT fk_craft_curve_category_id
+FOREIGN KEY(`category_id`) REFERENCES config_category(`id`);
+
+ALTER TABLE `craft_curve` ADD CONSTRAINT fk_craft_curve_steel_group_id
+FOREIGN KEY(`steel_group_id`) REFERENCES basic_steel_group(`id`);
 
 -- ============================================================
 -- 工艺工序
-
-ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_standard_craft_card_id
-FOREIGN KEY(`standard_craft_card_id`) REFERENCES info_craft(`id`);
-
-ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_basic_form_id
-FOREIGN KEY(`basic_form_id`) REFERENCES basic_form(`id`);
-
-ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_basic_curve_id
-FOREIGN KEY(`basic_curve_id`) REFERENCES basic_curve(`id`);
-
--- ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_info_craft_tempering_id
--- FOREIGN KEY(`info_craft_tempering_id`) REFERENCES info_craft_tempering(`id`);
-
-
-
-ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_product_craft_card_id
+-- ===========================================================
+-- 生产配料卡
+ALTER TABLE `card_ingredients` ADD CONSTRAINT fk_card_ingredients
 FOREIGN KEY(`product_craft_card_id`) REFERENCES card_craft(`id`);
 
-ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_basic_form_id
+-- 工艺工序
+-- ------------ info
+ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_info_craft_process_steel_group_id
+FOREIGN KEY(`steel_group_id`) REFERENCES basic_steel_group(`id`);
+
+ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_info_craft_process_standard_craft_card_id
+FOREIGN KEY(`standard_craft_card_id`) REFERENCES info_craft(`id`);
+
+ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_info_craft_process_basic_from_id
 FOREIGN KEY(`basic_form_id`) REFERENCES basic_form(`id`);
 
-ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_basic_curve_id
-FOREIGN KEY(`basic_curve_id`) REFERENCES basic_curve(`id`);
+ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_info_craft_process_basic_from_id
+FOREIGN KEY(`basic_form_id`) REFERENCES info_craft_fast(`id`);
 
--- ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_info_craft_tempering_id
--- FOREIGN KEY(`info_craft_tempering_id`) REFERENCES card_craft_tempering(`id`);
+ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_info_craft_process_basic_from_id
+FOREIGN KEY(`basic_form_id`) REFERENCES info_craft_diameter(`id`);
 
+ALTER TABLE `info_craft_process` ADD CONSTRAINT fk_info_craft_process_info_craft_tempering_id
+FOREIGN KEY(`info_craft_tempering_id`) REFERENCES info_craft_tempering(`id`);
+-- ----------- card 
+ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_card_craft_process_steel_group_id
+FOREIGN KEY(`steel_group_id`) REFERENCES basic_steel_group(`id`);
 
+ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_card_craft_process_product_craft_card_id
+FOREIGN KEY(`product_craft_card_id`) REFERENCES card_craft(`id`);
 
+ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_card_craft_process_basic_from_id
+FOREIGN KEY(`basic_form_id`) REFERENCES basic_form(`id`);
 
+ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_card_craft_process_basic_from_id
+FOREIGN KEY(`basic_form_id`) REFERENCES card_craft_fast(`id`);
 
--- 快锻
-ALTER TABLE `info_craft_fast` ADD CONSTRAINT fk_process_craft_id
-FOREIGN KEY(`process_craft_id`) REFERENCES info_craft_process(`id`);
+ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_card_craft_process_basic_from_id
+FOREIGN KEY(`basic_form_id`) REFERENCES card_craft_diameter(`id`);
 
-ALTER TABLE `card_craft_fast` ADD CONSTRAINT fk_process_craft_id
-FOREIGN KEY(`process_craft_id`) REFERENCES card_craft_process(`id`);
+ALTER TABLE `card_craft_process` ADD CONSTRAINT fk_card_craft_process_card_craft_tempering_id
+FOREIGN KEY(`card_craft_tempering_id`) REFERENCES card_craft_tempering(`id`);
+
+-- 修改记录
+ALTER TABLE `info_modify_record` ADD CONSTRAINT fk_info_modify_record_standard_craft_card_id
+FOREIGN KEY(`standard_craft_card_id`) REFERENCES info_craft(`id`);
+
+ALTER TABLE `card_modify_record` ADD CONSTRAINT fk_card_modify_record_product_craft_card_id
+FOREIGN KEY(`product_craft_card_id`) REFERENCES card_craft(`id`);
 
 -- 快断火次
-ALTER TABLE `info_craft_fast_order` ADD CONSTRAINT fk_craft_fast_id
+ALTER TABLE `info_craft_fast_order` ADD CONSTRAINT fk_info_craft_fast_order_craft_fast_id
 FOREIGN KEY(`craft_fast_id`) REFERENCES info_craft_fast(`id`);
 
-ALTER TABLE `card_craft_fast_order` ADD CONSTRAINT fk_craft_fast_id
+ALTER TABLE `card_craft_fast_order` ADD CONSTRAINT fk_card_craft_fast_order_craft_fast_id
 FOREIGN KEY(`craft_fast_id`) REFERENCES card_craft_fast(`id`);
 
+-- 径锻道次文件配置
+ALTER TABLE `info_craft_diameter_passfile` ADD CONSTRAINT fk_info_craft_diameter_passfile_process_craft_id
+FOREIGN KEY(`process_craft_id`) REFERENCES info_craft_diameter(`id`);
 
-
-
--- 径锻
-ALTER TABLE `info_craft_diameter` ADD CONSTRAINT fk_process_craft_id
-FOREIGN KEY(`process_craft_id`) REFERENCES info_craft_process(`id`);
-
-ALTER TABLE `card_craft_diameter` ADD CONSTRAINT fk_process_craft_id
-FOREIGN KEY(`process_craft_id`) REFERENCES card_craft_process(`id`);
-
-
-
+ALTER TABLE `card_craft_diameter_passfile` ADD CONSTRAINT fk_card_craft_diameter_passfile_process_craft_id
+FOREIGN KEY(`process_craft_id`) REFERENCES card_craft_diameter(`id`);
 
 -- 包装入库
-ALTER TABLE `info_craft_package` ADD CONSTRAINT fk_process_craft_id
+ALTER TABLE `info_craft_package` ADD CONSTRAINT fk_process_craft_idinfo_craft_package_process_craft_id
 FOREIGN KEY(`process_craft_id`) REFERENCES info_craft_process(`id`);
 
-ALTER TABLE `card_craft_package` ADD CONSTRAINT fk_process_craft_id
+ALTER TABLE `card_craft_package` ADD CONSTRAINT fk_process_craft_idcard_craft_package_process_craft_id
 FOREIGN KEY(`process_craft_id`) REFERENCES card_craft_process(`id`);
 
 
-
-ALTER TABLE `basic_curve_steel_group_relation` ADD CONSTRAINT fk_basic_curve_id
-FOREIGN KEY(`basic_curve_id`) REFERENCES basic_curve(`id`);
-
-ALTER TABLE `basic_curve_steel_group_relation` ADD CONSTRAINT fk_steeel_group_id
-FOREIGN KEY(`steeel_group_id`) REFERENCES basic_steel_group(`id`);
-
-
-ALTER TABLE `card_craft` ADD CONSTRAINT fk_product_ingredients_card_id
-FOREIGN KEY(`product_ingredients_card_id`) REFERENCES card_ingredients(`id`);
-
-
-
--- ===============================================================================
-ALTER TABLE `card_craft_tempering_modify_record` ADD CONSTRAINT fk_tempering_id_1
+-- ============================================================
+-- 调质工艺
+-- ============================================================
+-- 调质工艺卡修改记录
+ALTER TABLE `info_craft_tempering_modify_record` ADD CONSTRAINT fk_info_craft_tempering_modify_record_tempering_id
 FOREIGN KEY(`tempering_id`) REFERENCES info_craft_tempering(`id`);
 
-ALTER TABLE `card_tempering_surface_hardness` ADD CONSTRAINT fk_tempering_id_2
-FOREIGN KEY(`tempering_id`) REFERENCES info_craft_tempering(`id`);
-
-ALTER TABLE `card_tempering_performance` ADD CONSTRAINT fk_tempering_id_3
-FOREIGN KEY(`tempering_id`) REFERENCES info_craft_tempering(`id`);
-
-ALTER TABLE `card_tempering_keypoint` ADD CONSTRAINT fk_tempering_id_4
-FOREIGN KEY(`tempering_id`) REFERENCES info_craft_tempering(`id`);
-
-ALTER TABLE `card_tempering_keypoint_quenching` ADD CONSTRAINT tempering_keypoint_id_1
-FOREIGN KEY(`tempering_keypoint_id`) REFERENCES info_tempering_keypoint(`id`);
-
-ALTER TABLE `card_tempering_multiregulation_period` ADD CONSTRAINT fk_quenching_pc_id
-FOREIGN KEY(`quenching_pc_id`) REFERENCES info_tempering_keypoint_quenching(`id`);
-
-ALTER TABLE `card_tempering_multiregulation_period` ADD CONSTRAINT fk_tempering_pc_id
-FOREIGN KEY(`tempering_pc_id`) REFERENCES info_tempering_keypoint_tempering(`id`);
-
-ALTER TABLE `card_tempering_keypoint_tempering` ADD CONSTRAINT tempering_keypoint_id_2
-FOREIGN KEY(`tempering_keypoint_id`) REFERENCES info_tempering_keypoint(`id`);
-
-ALTER TABLE `card_tempering_keypoint_straightening` ADD CONSTRAINT tempering_keypoint_id_3
-FOREIGN KEY(`tempering_keypoint_id`) REFERENCES info_tempering_keypoint(`id`);
-
-
-
-
-
-ALTER TABLE `craft_curve` ADD CONSTRAINT fk_tempering_id_card
+ALTER TABLE `card_craft_tempering_modify_record` ADD CONSTRAINT fk_card_craft_tempering_modify_record_tempering_id
 FOREIGN KEY(`tempering_id`) REFERENCES card_craft_tempering(`id`);
 
-
-ALTER TABLE `craft_curve` ADD CONSTRAINT fk_tempering_id_info
+-- 表面硬度
+ALTER TABLE `info_tempering_surface_hardness` ADD CONSTRAINT fk_info_tempering_surface_hardness_tempering_id
 FOREIGN KEY(`tempering_id`) REFERENCES info_craft_tempering(`id`);
 
+ALTER TABLE `card_tempering_surface_hardness` ADD CONSTRAINT fk_card_tempering_surface_hardness_tempering_id
+FOREIGN KEY(`tempering_id`) REFERENCES card_craft_tempering(`id`);
+
+-- 力学性能要求
+ALTER TABLE `info_tempering_performance` ADD CONSTRAINT fk_info_tempering_performance_tempering_id
+FOREIGN KEY(`tempering_id`) REFERENCES info_craft_tempering(`id`);
+
+ALTER TABLE `card_tempering_performance` ADD CONSTRAINT fk_card_tempering_performance_tempering_id
+FOREIGN KEY(`tempering_id`) REFERENCES card_craft_tempering(`id`);
+
+-- 工艺要点表
+ALTER TABLE `info_tempering_keypoint` ADD CONSTRAINT fk_info_tempering_keypoint_tempering_id
+FOREIGN KEY(`tempering_id`) REFERENCES info_craft_tempering(`id`);
+
+ALTER TABLE `card_tempering_keypoint` ADD CONSTRAINT fk_card_tempering_keypoint_tempering_id
+FOREIGN KEY(`tempering_id`) REFERENCES card_craft_tempering(`id`);
+
+-- 工艺要点-淬火
+ALTER TABLE `info_tempering_keypoint_quenching` ADD CONSTRAINT fk_info_tempering_keypoint_quenching_tempering_keypoint_id
+FOREIGN KEY(`tempering_keypoint_id`) REFERENCES info_tempering_keypoint(`id`);
+
+ALTER TABLE `card_tempering_keypoint_quenching` ADD CONSTRAINT fk_card_tempering_keypoint_quenching_tempering_keypoint_id
+FOREIGN KEY(`tempering_keypoint_id`) REFERENCES card_tempering_keypoint(`id`);
+
+-- 工艺要点-回火
+ALTER TABLE `info_tempering_keypoint_tempering` ADD CONSTRAINT fk_info_tempering_keypoint_tempering_tempering_keypoint_id
+FOREIGN KEY(`tempering_keypoint_id`) REFERENCES info_tempering_keypoint(`id`);
+
+ALTER TABLE `card_tempering_keypoint_tempering` ADD CONSTRAINT fk_card_tempering_keypoint_tempering_tempering_keypoint_id
+FOREIGN KEY(`tempering_keypoint_id`) REFERENCES card_tempering_keypoint(`id`);
 
 
-ALTER TABLE `craft_curve` ADD CONSTRAINT fk_basic_curve_quenching_id
-FOREIGN KEY(`basic_curve_quenching_id`) REFERENCES basic_curve(`id`);
+-- 工艺要点-矫直
+ALTER TABLE `info_tempering_keypoint_straightening` ADD CONSTRAINT fk_info_tempering_keypoint_straightening_tempering_keypoint_id
+FOREIGN KEY(`tempering_keypoint_id`) REFERENCES info_tempering_keypoint(`id`);
+
+ALTER TABLE `card_tempering_keypoint_straightening` ADD CONSTRAINT fk_card_tempering_keypoint_straightening_tempering_keypoint_id
+FOREIGN KEY(`tempering_keypoint_id`) REFERENCES card_tempering_keypoint(`id`);
 
 
-ALTER TABLE `craft_curve` ADD CONSTRAINT fk_basic_curve_tempering_id
-FOREIGN KEY(`basic_curve_tempering_id`) REFERENCES basic_curve(`id`);
-
-
-ALTER TABLE `craft_curve` ADD CONSTRAINT fk_basic_curve_normalizing_id
-FOREIGN KEY(`basic_curve_normalizing_id`) REFERENCES basic_curve(`id`);
-
-
-
-
-ALTER TABLE `info_tempering_multiregulation_period` ADD CONSTRAINT fk_quenching_pc_id_info
+-- 多规格
+-------------
+ALTER TABLE `info_tempering_multiregulation_period` ADD CONSTRAINT fk_info_tempering_multiregulation_period_quenching_pc_id
 FOREIGN KEY(`quenching_pc_id`) REFERENCES info_tempering_keypoint_quenching(`id`);
 
-
-
-ALTER TABLE `info_tempering_multiregulation_period` ADD CONSTRAINT fk_tempering_pc_id_info
+ALTER TABLE `info_tempering_multiregulation_period` ADD CONSTRAINT fk_info_tempering_multiregulation_period_tempering_pc_id
 FOREIGN KEY(`tempering_pc_id`) REFERENCES info_tempering_keypoint_tempering(`id`);
 
-
-
-
-ALTER TABLE `card_tempering_multiregulation_period` ADD CONSTRAINT fk_quenching_pc_id_card
+ALTER TABLE `card_tempering_multiregulation_period` ADD CONSTRAINT fk_card_tempering_multiregulation_period_quenching_pc_id
 FOREIGN KEY(`quenching_pc_id`) REFERENCES card_tempering_keypoint_quenching(`id`);
 
-
-ALTER TABLE `card_tempering_multiregulation_period` ADD CONSTRAINT fk_tempering_pc_id_card
+ALTER TABLE `card_tempering_multiregulation_period` ADD CONSTRAINT fk_card_tempering_multiregulation_period_tempering_pc_id
 FOREIGN KEY(`tempering_pc_id`) REFERENCES card_tempering_keypoint_tempering(`id`);
 
--- xyg/result/xyg_2020-09-30 14:36:38.sql
+++ xyg/result/xyg_2020-09-30 14:36:38.sql
@@ -6,20 +6,20 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- =================================================================
 DROP TABLE IF EXISTS `basic_curve_menu`;
 CREATE TABLE `basic_curve_menu` (
-    `id` INT unsigned NOT NULL AUTO_INCREMENT COMMENT '基础曲线树形菜单ID',
-    `basic_curve_id` INT unsigned NULL DEFAULT NULL COMMENT '基础曲线表ID',
-    `parent_id` INT unsigned NULL DEFAULT NULL COMMENT '父ID',
+    `id` INT NOT NULL AUTO_INCREMENT COMMENT '基础曲线树形菜单ID',
+    `basic_curve_id` INT NULL DEFAULT NULL COMMENT '基础曲线表ID',
+    `parent_id` INT NULL DEFAULT NULL COMMENT '父ID',
     `node_name` VARCHAR(255) NULL DEFAULT NULL COMMENT '节点名称',
     `upper_limit` DOUBLE(10,5) NULL DEFAULT NULL COMMENT '上限',
     `lower_limit` DOUBLE(10,5) NULL DEFAULT NULL COMMENT '下限',
-    `category_id` INT unsigned NULL DEFAULT NULL COMMENT '基础分类ID',
+    `category_id` INT NULL DEFAULT NULL COMMENT '基础分类ID',
     `created_user` VARCHAR(255) NOT NULL  COMMENT '创建者',
     `updated_user` VARCHAR(255) NULL DEFAULT NULL COMMENT '更新者',
     `deleted_user` VARCHAR(255) NULL DEFAULT NULL COMMENT '删除者',
     `created_datetime` DATETIME(6) NOT NULL  COMMENT '创建日期',
     `updated_datetime` DATETIME(6) NULL DEFAULT NULL COMMENT '更新日期',
     `deleted_datetime` DATETIME(6) NULL DEFAULT NULL COMMENT '删除日期',
-    `deleted` TINYINT unsigned NOT NULL DEFAULT 0 COMMENT '删除标记',
+    `deleted` TINYINT NOT NULL DEFAULT 0 COMMENT '删除标记',
     PRIMARY KEY (`id`) USING BTREE
     -- CONSTRAINT `foreign_key_name` FOREIGN KEY (`foreign_key`) REFERENCES `master_table`(`master_table_primary_key`),
     -- INDEX (`index`)
@@ -32,8 +32,8 @@ SET FOREIGN_KEY_CHECKS = 1;
 -- =================================================================
 DROP TABLE IF EXISTS `info_craft_diameter_passfile`;
 CREATE TABLE `info_craft_diameter_passfile` (
-    `id` INT unsigned NOT NULL AUTO_INCREMENT COMMENT '径锻道次文件配置ID',
-    `process_craft_id` INT unsigned NULL DEFAULT NULL COMMENT '基础表格-径锻ID',
+    `id` INT NOT NULL AUTO_INCREMENT COMMENT '径锻道次文件配置ID',
+    `process_craft_id` INT NULL DEFAULT NULL COMMENT '基础表格-径锻ID',
     `supplied_material_type` VARCHAR(255) NOT NULL  COMMENT '来料类型',
     `supplied_material_size` DOUBLE(10,5) NOT NULL  COMMENT '来料尺寸',
     `material_norm_min` DOUBLE(10,5) NOT NULL  COMMENT '成材规格_下限',
@@ -53,7 +53,7 @@ CREATE TABLE `info_craft_diameter_passfile` (
     `created_datetime` DATETIME(6) NOT NULL  COMMENT '创建日期',
     `updated_datetime` DATETIME(6) NULL DEFAULT NULL COMMENT '更新日期',
     `deleted_datetime` DATETIME(6) NULL DEFAULT NULL COMMENT '删除日期',
-    `deleted` TINYINT unsigned NOT NULL DEFAULT 0 COMMENT '删除标记',
+    `deleted` TINYINT NOT NULL DEFAULT 0 COMMENT '删除标记',
     PRIMARY KEY (`id`) USING BTREE
     -- CONSTRAINT `foreign_key_name` FOREIGN KEY (`foreign_key`) REFERENCES `master_table`(`master_table_primary_key`),
     -- INDEX (`index`)
@@ -66,8 +66,8 @@ SET FOREIGN_KEY_CHECKS = 1;
 -- =================================================================
 DROP TABLE IF EXISTS `card_craft_diameter_passfile`;
 CREATE TABLE `card_craft_diameter_passfile` (
-    `id` INT unsigned NOT NULL AUTO_INCREMENT COMMENT '径锻道次文件配置ID',
-    `process_craft_id` INT unsigned NULL DEFAULT NULL COMMENT '基础表格-径锻ID',
+    `id` INT NOT NULL AUTO_INCREMENT COMMENT '径锻道次文件配置ID',
+    `process_craft_id` INT NULL DEFAULT NULL COMMENT '基础表格-径锻ID',
     `supplied_material_type` VARCHAR(255) NOT NULL  COMMENT '来料类型',
     `supplied_material_size` DOUBLE(10,5) NOT NULL  COMMENT '来料尺寸',
     `material_norm_min` DOUBLE(10,5) NOT NULL  COMMENT '成材规格_下限',
@@ -87,7 +87,7 @@ CREATE TABLE `card_craft_diameter_passfile` (
     `created_datetime` DATETIME(6) NOT NULL  COMMENT '创建日期',
     `updated_datetime` DATETIME(6) NULL DEFAULT NULL COMMENT '更新日期',
     `deleted_datetime` DATETIME(6) NULL DEFAULT NULL COMMENT '删除日期',
-    `deleted` TINYINT unsigned NOT NULL DEFAULT 0 COMMENT '删除标记',
+    `deleted` TINYINT NOT NULL DEFAULT 0 COMMENT '删除标记',
     PRIMARY KEY (`id`) USING BTREE
     -- CONSTRAINT `foreign_key_name` FOREIGN KEY (`foreign_key`) REFERENCES `master_table`(`master_table_primary_key`),
     -- INDEX (`index`)
